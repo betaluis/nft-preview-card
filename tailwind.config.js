85d17dfe179c871bc285a22874cc11023366e388
betaluis <luis.betancourt.dev@gmail.com>
2022-07-26 11:02:29 +0000

    completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,26 @@
+const plugin = require('tailwindcss/plugin');
+
+const rotateX = plugin(function ({ addUtilities }) {
+  addUtilities({
+    '.rotate-x-20': {
+      transform: 'rotateX(20deg)',
+    },
+    '.rotate-x-40': {
+      transform: 'rotateX(40deg)',
+    },
+    '.rotate-x-60': {
+      transform: 'rotateX(60deg)',
+    },
+    '.rotate-x-80': {
+     transform: 'rotateX(80deg)',
+    },
+    '.rotate-x-90': {
+     transform: 'rotateX(90deg)',
+    },
+  })
+}) 
+
+
 module.exports = {
   content: [
     "./src/**/*.{html,js,ts,jsx,tsx}",
@@ -7,6 +30,7 @@ module.exports = {
       colors: {
         primary: 'hsl(215, 51%, 70%)',
         secondary: 'hsl(178, 100%, 50%)',
+        'secondary-transparent': 'rgba(0, 255, 247, .60)',
         'dark-blue-100': 'hsl(217, 54%, 11%)',
         'dark-blue-200': 'hsl(216, 50%, 16%)',
         'dark-blue-300': 'hsl(215, 32%, 27%)',
@@ -17,5 +41,5 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  plugins: [rotateX],
 }
